2491e5c52e70370f9dc754c743b9d91ff2229a58
maeliuliu <maeganl76@gmail.com>
2025-07-29 14:30:47 -0700

    added one line

--- UTime/NewToDo.swift
+++ UTime/NewToDo.swift
@@ -13,6 +13,7 @@ struct NewToDo: View {
         VStack{
             Text("Task title:")
             TextField("Enter task description", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .cornerRadius(15)
             Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
                 Text("Is it important?")
             }
--- UTime/ToDoPage.swift
+++ UTime/ToDoPage.swift
@@ -38,6 +38,7 @@ struct ToDoPage: View {
     }
 }
 
+
 #Preview {
     ToDoPage()
 }

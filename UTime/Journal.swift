0fc0ca8a79b4a6c1921958500e980058ee6b0da9
Chaylen <chaylennguyentran@gmail.com>
2025-07-29 13:09:05 -0700

    text and buttons

--- UTime/Journal.swift
+++ UTime/Journal.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct Journal: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Journal")
+            .font(.title)
+            .fontWeight(.bold)
+        Text("where you can clear your head and write down your thoughts ✨")
+        Button("New journal entry"){
+            
+        }
+        
     }
 }
 

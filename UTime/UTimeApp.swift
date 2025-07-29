650be59f4f7ba0c8546584d94a5990c8ab97721d
Chaylen <chaylennguyentran@gmail.com>
2025-07-29 11:15:51 -0700

    Initial Commit

---
+++ UTime/UTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTimeApp.swift
+//  UTime
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct UTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

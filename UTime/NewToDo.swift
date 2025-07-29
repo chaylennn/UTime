665388df71dcbdd222c0adb171e00fc3a05b0674
maeliuliu <maeganl76@gmail.com>
2025-07-29 12:00:32 -0700

    made changes to the to do view + added a new view for to do list

---
+++ UTime/NewToDo.swift
@@ -0,0 +1,31 @@
+//
+//  NewToDo.swift
+//  UTime
+//
+//  Created by maegan on 7/29/25.
+//
+
+//this is for the to do list btw
+import SwiftUI
+
+struct NewToDo: View {
+    var body: some View {
+        VStack{
+            Text("Task title:")
+            TextField("Enter task description", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            Button() {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            } label: { Text("Save")
+            }
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDo()
+}

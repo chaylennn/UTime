bd3878393a4ccfc9baeb09d1c752b77132280a62
Anvi <anvimishra09@gmail.com>
2025-07-29 11:39:35 -0700

    resources file

---
+++ UTime/Resources.swift
@@ -0,0 +1,18 @@
+//
+//  Resources.swift
+//  UTime
+//
+//  Created by anvi mishra on 7/29/25.
+//
+
+import SwiftUI
+
+struct Resources: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Resources()
+}

882f5079fd7c7c569b53b6def81615bddd15e220
maeliuliu <maeganl76@gmail.com>
2025-07-31 09:58:46 -0700

    added color theme

---
+++ UTime/customization/appColor.swift
@@ -0,0 +1,20 @@
+//
+//  appColor.swift
+//  UTime
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum AppColorTheme{
+    static let brownColor: Color = Color("dark brown")
+    static let creamColor: Color = Color("cream")
+    static let blueColor: Color = Color("blue")
+    static let lightBrownColor: Color = Color("light brown")
+    static let lightGreenColor: Color = Color("light green")
+    static let darkGreenColor: Color = Color("dark green")
+}
+
+
---
+++ UTime/to do list/ToDoPage.swift
@@ -0,0 +1,77 @@
+//
+//  ToDoPage.swift
+//  UTime
+//
+//  Created by Jolin Wang on 7/29/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct ToDoPage: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    
+    var body: some View {
+        ZStack{
+            Color(AppColorTheme.brownColor)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                //This is the to do list title and the + button
+                HStack{
+                    Text("To-Do List")
+                        .font(.system(size:40))
+                        .fontWeight(.black)
+                        .foregroundColor(AppColorTheme.creamColor)
+                    Spacer()
+                    Button() {
+                        withAnimation{
+                            showNewTask = true
+                        }
+                    } label: { Text("+")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(AppColorTheme.creamColor)
+                    }
+                }
+                .padding()
+                Spacer()
+                
+                //add a list for the tasks
+                List{
+                    ForEach(toDos){ toDoItem in
+                        if toDoItem.isImportant{
+                            Text("!!" + toDoItem.title)
+                        } else {
+                            Text(toDoItem.title)
+                        }
+                    }
+                    .onDelete(perform: deleteToDo)
+                }
+            }
+        }
+        if showNewTask{
+            NewToDo(showNewTask: $showNewTask, toDoItem: ToDoItem(title:"", isImportant: false))
+        }
+        
+    }
+    
+    //adding the delete task function
+    
+    func deleteToDo(at offsets: IndexSet){
+        for offset in offsets{
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
+            
+        }
+    }
+    
+}
+
+
+#Preview {
+    ToDoPage()
+        .modelContainer(for: ToDoItem.self)
+}
